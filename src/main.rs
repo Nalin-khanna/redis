98d239290c3cee16b76e745f87df0fd27fd59676
Nalin-khanna <nalinkhanna17@gmail.com>
2025-10-04 09:07:29 +0530

    Port binding

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![allow(unused_imports)]
+use std::net::TcpListener;
+
+fn main() {
+    println!("Hello, world!");
+    
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
---
+++ target/debug/deps/redis_rs-2d37b5849347ccd8.d
@@ -0,0 +1,5 @@
+/Users/nalinkhanna/Documents/rust_projects/redis-rs/target/debug/deps/redis_rs-2d37b5849347ccd8.d: src/main.rs
+
+/Users/nalinkhanna/Documents/rust_projects/redis-rs/target/debug/deps/libredis_rs-2d37b5849347ccd8.rmeta: src/main.rs
+
+src/main.rs:
