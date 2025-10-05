a07644412c8c59e37df85e874ccb087f4b5bbd39
Nalin-khanna <nalinkhanna17@gmail.com>
2025-10-05 17:12:23 +0530

    responding to multiple ping

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::{Read, Write}, net::TcpListener};
 
 fn main() {
     println!("Hello, world!");
-    
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let mut buffer = [0;1024];
+                let n = _stream.read(& mut buffer);
+                match n {
+                    Ok(n) => {
+                        _stream.write_all(b"+PONG\r\n").unwrap();
+                    }
+                    Err(err) => {
+                        print!("error : {} " , err);
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
