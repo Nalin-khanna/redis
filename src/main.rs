fe8be1d09064ec21913a9ae61aee8c88245e97c1
Nalin-khanna <nalinkhanna17@gmail.com>
2025-10-15 21:49:52 +0530

    handling concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
 #![allow(unused_imports)]
-use std::{io::{Read, Write}, net::TcpListener};
-
-fn main() {
-    println!("Hello, world!");
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                println!("accepted new connection");
-                let mut buffer = [0;1024];
-                loop {
-                    let n = _stream.read(&mut buffer).unwrap();
-                    if n == 0 {
-                        break;
-                    }
-                    _stream.write_all(b"+PONG\r\n").unwrap();
+use std::{io::{Read, Write}};
+use tokio::{io::{AsyncReadExt , AsyncWriteExt}, net::{TcpListener,TcpStream}};
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let (mut stream , _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            let mut buffer = [0u8 ; 1024];
+            loop {
+                let n = stream.read(&mut buffer).await.unwrap();
+                if n == 0 {break;}
+                if let Err(e) = stream.write_all(b"+PONG\r\n").await {
+                    println!("error {}", e);
                 }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
